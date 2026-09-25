949adb114eda97fd2c06940c59e9721a28491359
reinj36 <rjames36@myseneca.ca>
2026-09-25 16:35:22 +0000

    Completed Lab 2c in class.

--- lab2c.py
+++ lab2c.py
@@ -2,15 +2,19 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Reina James
+# Date: 25/9/2026
 # Purpose: Practice using if, elif, and else statments.
 # Usage: ./lab2c.py
 
 # TO DO 1:
 # Prmopt the user to enter a sentence, save it in the variable str1
 # Prmopt the user to enter another sentence, save it in the variable str2
-#
+
+#get user input
+str1 = input("Enter a sentence: ")
+str2 = input("Enter another sentence: ")
+
 # Use if, elif, and else statments with the len() function to check which of the 2 is longer.
 # The final result should be:
 # ---- is longer then ----
@@ -18,5 +22,11 @@
 # ---- and ---- are equal.
 # Get input from the user
 
+if (len(str1) > len(str2)) :
+    print ("str1 is longer than str2!")
+elif (len(str2) > len(str1)) :
+    print("str2 is longer than str1!")
+else :
+    "str1 and str2 are equal."
 
 
--- lab2d.py
+++ lab2d.py
@@ -6,9 +6,14 @@
 # Purpose: .
 # Usage: ./lab2d.py
 
-
 # TO DO 1: copy the required lines from README.md to print version, platform, argv and the length of argv.
 # run the script in the terminal using command: python ./lab2d.py
+
+import sys
+print(sys.version)
+print(sys.platform)
+print(sys.argv)
+print(len(sys.argv))
 
 # TO DO 2: copy the required lines from README.md to print argv[0], argv[1] and argv[2]
 # run the script using the following command: python lab2d.py maija Maija
