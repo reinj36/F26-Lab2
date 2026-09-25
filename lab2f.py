18eba0799fb286586ae964c768000c0032e45334
reinj36 <rjames36@myseneca.ca>
2026-09-25 17:10:48 +0000

    Completed Lab 2f in class.

--- lab2f.py
+++ lab2f.py
@@ -1,9 +1,23 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Reina James
+# Date: 25/9/2026
 # Purpose: Learn how to use command line arguments.
 # Usage: ./lab2f.py
 
 # TO DO 1: Follow the instructions given in README.md file
+
+import sys
+num_args = len(sys.argv) -1
+               
+if num_args < 2 :
+    print("The script requires at least 2 arguments.")
+else :
+    name = sys.argv[1]
+    age = sys.argv[2]
+
+    if num_args == 3 :
+        print(f"Hi {name}, you are {age} years old and the script received {num_args} arguments.")
+    else :
+        print(f"Hi {name}, you are {age} years old and the script recieved {num_args} arguments.")
