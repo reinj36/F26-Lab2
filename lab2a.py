474f0b85adaa3206220a71503acc21868f9ac503
reinj36 <rjames36@myseneca.ca>
2026-09-25 16:20:42 +0000

    Lab 2a completed in class.

--- lab2a.py
+++ lab2a.py
@@ -1,13 +1,24 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Reina James
+# Date: 25/9/2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: ./lab2a.py
 
 # TO DO 1: Follow the instructions given in README.md file
 
+#assign user input to x
+x = input("Please input a number: ")
 
+#check type of x
+type(x) 
 
+#convert x to int
+x = int(x)
 
+if (x >= 6) :
+    print("x is greater than 6!")
+
+if (x >= 4 and x < 12) :
+    print("x is greater than 4 and less than 12!")
